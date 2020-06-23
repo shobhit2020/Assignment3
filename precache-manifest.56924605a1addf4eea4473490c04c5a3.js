4ea951f9644c802ea7e6faccdfbd51648085adb0
shobhit2020 <triapthi.samy@gmail.com>
2020-06-23 18:14:43 +0530

    Updates

---
+++ precache-manifest.56924605a1addf4eea4473490c04c5a3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e97e5e114b38371cfd5372e44b56c720",
+    "url": "/Assignment3/index.html"
+  },
+  {
+    "revision": "b002b77251f5cfb68b89",
+    "url": "/Assignment3/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "18cfd4bd1dc8fe4cabc6",
+    "url": "/Assignment3/static/css/main.c150feb4.chunk.css"
+  },
+  {
+    "revision": "b002b77251f5cfb68b89",
+    "url": "/Assignment3/static/js/2.167bb158.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Assignment3/static/js/2.167bb158.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18cfd4bd1dc8fe4cabc6",
+    "url": "/Assignment3/static/js/main.3f1b0dbc.chunk.js"
+  },
+  {
+    "revision": "5fd29e0a731affb34a60",
+    "url": "/Assignment3/static/js/runtime-main.94e9321c.js"
+  },
+  {
+    "revision": "2d51e2b014429304b4b9bb7dd4865161",
+    "url": "/Assignment3/static/media/embedded.2d51e2b0.jpg"
+  },
+  {
+    "revision": "878c280b8766df562911ac825f5fdd75",
+    "url": "/Assignment3/static/media/pds.878c280b.jpg"
+  }
+]);
